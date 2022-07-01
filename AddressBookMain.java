aa03781ba373cd65ec38ea46c150d6db1ff34da3
“itsmeanubhav” <imanubhav074@gmail.com>
2022-07-02 01:47:05 +0530

    Added : Welcome Message

---
+++ AddressBookMain.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+    //Displaying Welcome Message
+public class AddressBookMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address Book Program");
+    }
+}
